4514298a07bf83d0b177e9a186eb7f230c4a0a34
RayVanHorn <61771343+RayVanHorn@users.noreply.github.com>
2023-01-29 19:18:39 -0800

    Reworked Camera Controller
    Added Q/E rotation control to camera, which rotates it around the player.
    Camera X/Y still follows the player. Player movement is unchanged.

--- Project 0/Roll-a-Ball/Assets/Scripts/CameraController.cs
+++ Project 0/Roll-a-Ball/Assets/Scripts/CameraController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    public Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,19 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        // Keep us at the last known relative position
+        transform.position = (player.transform.position + offset);
+        if (Input.GetKey("q"))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, 45 * Time.deltaTime);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, -45 * Time.deltaTime);
+        }
+
+
+        // Update new offset after rotation mucks w/ the coordinates
+        offset = transform.position - player.transform.position;
     }
 }
